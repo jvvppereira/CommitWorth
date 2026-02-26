71904eca7a28a71af474b4e58aa8c9e547552de7
João Vítor <joao.vitor.paes@hotmail.com>
2026-02-26 16:24:07 -0300

    Update calcCommitStarsForks.ts

--- src/lib/calcs/calcCommitStarsForks.ts
+++ src/lib/calcs/calcCommitStarsForks.ts
@@ -4,7 +4,7 @@ import { calculateUnifiedCommitCount } from "./calculateUnifiedCommitCount"
 export function calcCommitStarsForks(data: GitHubStatsResponse, repos: Repository[]) {
     const totalStars = repos.reduce((total, repo) => total + (repo.stargazerCount ?? 0), 0)
     const totalForks = repos.reduce((total, repo) => total + (repo.forkCount ?? 0), 0)
-    const totalCommits = calculateUnifiedCommitCount(data, repos)
+    const totalCommits = calculateUnifiedCommitCount(data)
 
     return { totalCommits, totalStars, totalForks }
 }
