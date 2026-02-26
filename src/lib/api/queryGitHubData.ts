832599ae93042562e83d484669f515aed1657af4
João Vítor <joao.vitor.paes@hotmail.com>
2026-02-26 15:11:07 -0300

    Update queryGitHubData.ts

--- src/lib/api/queryGitHubData.ts
+++ src/lib/api/queryGitHubData.ts
@@ -2,18 +2,41 @@ import { gql } from "graphql-request";
 
 export const queryGitHubData = gql`
 query getUserStats($login: String!) {
+  rateLimit {
+    remaining
+    resetAt
+  }
+
   user(login: $login) {
+    id
     login
-    
+    name
+    avatarUrl
+    createdAt
+
+    repositories(first: 20, ownerAffiliations: OWNER) {
+      nodes {
+        name
+        stargazerCount
+        forkCount
+        isFork
+        createdAt
+        languages(first: 5) {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+
     contributionsCollection(
-      from: "2025-01-01T00:00:00Z"
+      from: "2015-01-01T00:00:00Z"
       to: "2025-12-31T23:59:59Z"
     ) {
       totalCommitContributions
       totalPullRequestContributions
       totalIssueContributions
       totalRepositoryContributions
-      totalRepositoriesWithContributedCommits
     }
   }
 }
