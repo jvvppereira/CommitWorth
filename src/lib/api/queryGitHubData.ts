c31a3fb20efb9004fe4a670506fcaa31bbe11743
João Vítor <joao.vitor.paes@hotmail.com>
2026-02-26 14:50:03 -0300

    Update queryGitHubData.ts

--- src/lib/api/queryGitHubData.ts
+++ src/lib/api/queryGitHubData.ts
@@ -1,70 +1,44 @@
 import { gql } from "graphql-request";
 
 export const queryGitHubData = gql`
-    query getUserStats($login: String!) {
-        rateLimit {
-            limit
-            remaining
-            resetAt
-        }
-        user(login: $login) {
-            id
-            login
+query getUserStats($login: String!) {
+  rateLimit {
+    remaining
+    resetAt
+  }
+
+  user(login: $login) {
+    id
+    login
+    name
+    avatarUrl
+    createdAt
+
+    repositories(
+      first: 50
+      ownerAffiliations: OWNER
+      orderBy: { field: STARGAZERS, direction: DESC }
+    ) {
+      nodes {
+        name
+        stargazerCount
+        forkCount
+        isFork
+        createdAt
+
+        languages(first: 5) {
+          nodes {
             name
-            avatarUrl
-            createdAt
-            repositories(first: 100, ownerAffiliations: OWNER) {
-                nodes {
-                    name
-                    stargazerCount
-                    forkCount
-                    isFork
-                    description
-                    homepageUrl
-                    hasIssuesEnabled
-                    createdAt
-                    languages(first: 10) {
-                        nodes {
-                            name
-                        }
-                    }
-                    defaultBranchRef {
-                        target {
-                            ... on Commit {
-                                history {
-                                    totalCount
-                                }
-                            }
-                        }
-                    }
-                    mentionableUsers(first: 10) {
-                        totalCount
-                    }
-                }
-            }
-            contributionsCollection {
-                contributionCalendar {
-                    totalContributions
-                    weeks {
-                        contributionDays {
-                            contributionCount
-                            date
-                        }
-                    }
-                }
-                commitContributionsByRepository {
-                    repository {
-                        name
-                    }
-                    contributions(first: 100) {
-                        totalCount
-                        nodes {
-                            occurredAt
-                            commitCount
-                        }
-                    }
-                }
-            }
+          }
         }
+      }
     }
+
+    contributionsCollection {
+      contributionCalendar {
+        totalContributions
+      }
+    }
+  }
+}
 `
